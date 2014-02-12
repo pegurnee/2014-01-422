ee9dd340ffb802e2a6b01d66050d1ee8412f1893
BlueShoelaces <narruda@emich.edu>
2014-02-12 02:57:47 -0500

    Fix a dumb mistake. Clarify and perfect timing.

--- Project2/p2_transmitter.asm
+++ Project2/p2_transmitter.asm
@@ -13,9 +13,9 @@
 cblock 	0x20 			;start of general purpose registers
 		var1			;var1 is symbolic name for location 0x20 - <use>
 		var2 			;var2 is symbolic name for location 0x21 - <use>
-		countp			;a counter w/in Poll loop
-		counts			;a counter w/in Send loop
-		countd			;a counter w/in Delay loop
+		countp			;counter w/in Poll loop
+		counts			;counter w/in Send loop
+		countd			;counter w/in Delay loop
 		number
 	endc
 
@@ -29,95 +29,111 @@ cblock 	0x20 			;start of general purpose registers
 	movlw	0x00
 	movwf	TRISB			;PORTB is output
 	movlw	b'11000011'
-	movwf	TRISA			;PORTA is input
+	movwf	TRISA			;RA0, RA1, RA6, and RA7 are input
 	bcf		STATUS,RP0		;return to bank 0
 
-;actual code follows here
+;PROGRAM BODY
+;While low, poll RA6 every 50µs. If high, read input,
+;transmit, delay, and continue.
+;(50µs = 2µs + 3µs + 43µs + 2µs)
 
-	;every ~53µs, poll RA6
-	;if high, read input and transmit
-Poll_it
-	btfsc	PORTA,0x06
-	call	Read_it		;if RA6 high, reads input
-	bsf		PORTA,0x02	;stop bit
-	movlw	0x10
-	movwf	countp
-Delay_poll
-	decfsz	countp,	f
-	goto	Delay_poll
-	goto	Poll_it
+	bsf		PORTA, 0x02	;default transmit bit is high
 	
+Poll_it
+	;2µs (if RA6 low):
+	btfsc	PORTA,0x06	;if RA6 low, delay and repeat
+	call	Read_it		;if RA6 high, read input
+
+	;3µs:
+	movlw	d'8'
+	movwf	countd		;counter for Delay_it
+	nop
+	
+	;43µs (2µs + (5µs * 8) + 1µs)
+	call	Delay_it
+	
+	;2µs:
+	goto	Poll_it
+
+;SUBROUTINE Read_it
+;Clears 'number' register. Sets each bit if
+;corresponding input is high.
 Read_it
 	clrf	number
 	
-	;PORTA bits 0, 1, and 7 take input
-	;significance: 1, 0, 7
-	btfsc	PORTA,0x01
-	bsf		number,b'00000100'
-	
-	btfsc	PORTA,0x00
-	bsf		number,b'00000010'
-	
-	btfsc	PORTA,0x07
-	bsf		number,b'00000001'
-	
-	;input stored in number, e.g. 00000111
-	;transmit through RA2
-	;high-order bit first
-	
-	movlw	0x08		;send a byte one bit at a time
-	movwf	counts
-	
-	bcf		PORTA,0x02	;start bit
+	;Relate PORTA inputs to bits in 'number.'
 
-	call	Delay_it	;delay a clock cycle
+	btfsc	PORTA, 0x01	;high-order bit
+	bsf		number, 0x02
 	
+	btfsc	PORTA, 0x00	;middle bit
+	bsf		number, 0x01
+	
+	btfsc	PORTA, 0x07	;low-order bit
+	bsf		number, 0x00
+	
+	;Input is stored in 'number' (7 = 00000111).
+	;Transmit byte through RA2, high-order bit first.
+	
+	movlw	0x08
+	movwf	counts		;counter for Send_it (8 bits)
+	
+	bcf		PORTA,0x02	;set start bit (low)
+	
+	;Wait 104µs before transmission of first bit
+	;(4µs + 98µs + 2µs)
+	
+	;4µs
+	movlw	d'19'
+	movwf	countd		;counter for Delay_it
+	nop
+	nop
+	
+	;98µs (2 µs + (5µs * 19) + 1µs):
+	call	Delay_it
+	
+	;2µs
+	call	Send_it
+	
+	bsf		PORTA, 0x02	;set stop bit (high)
+	return				;return from Read_it
+
+;SUBROUTINE Send_it
+;Each iteration of Send_it takes 104µs
+;(5µs + 3µs + 93µs + 3µs)
 Send_it
+	;5µs:
 	rlf		number, f	;shift bits left
-	btfsc	STATUS, C	;if carry bit = 1	
-	bsf		PORTA,0x02	;set RA2
-	btfss	STATUS, C	;if carry bit = 0
-	bcf		PORTA,0x02	;clear RA2
+	btfsc	STATUS, C	;if carry bit = 1,	
+	bsf		PORTA,0x02	;		set RA2.
+	btfss	STATUS, C	;if carry bit = 0,
+	bcf		PORTA,0x02	;		clear RA2.
 
-	call	Delay_it	;delay a clock cycle
+	;3µs:
+	movlw	d'18'
+	movwf	countd		;counter for Delay_it
+	nop
+	
+	;93µs (2µs + (5µs * 18) + 1µs):
+	call	Delay_it
 
+	;3µs (not including return):
 	decfsz	counts		;decrement count of 8 bits
 	goto	Send_it
-	return				;return to Read_it
+	
+	return				;return from Send_it
 
+;SUBROUTINE Delay_it
+;Total delay here, including return, is according
+;to contents of countd: (5µs * countd) + 1µs
 Delay_it
-	;delay 104us (really 97?) ????
-	movlw	d'20'
-	movwf	countd
-
-Delay
+	;5µs (+1µs on last pass):
 	nop
 	nop
 	decfsz	countd
-	goto	Delay
+	goto	Delay_it
 
-	return
+	return				;return from Delay_it
 
-	;delay 250ms
-;	movlw	d'250'
-;	movwf	countd
-
-;Delay_it
-;	movlw	d'200'
-;	movwf	count1m
-;	call	Delay_1
-;	decfsz	countd
-;	goto	Delay_it
-
-;	decfsz	counts
-;	goto	Send_it
-;	retlw	0x00	;after transmitting, return to Poll_it
-
-;Delay_1
-;	nop
-;	nop
-;	decfsz	count1m
-;	goto	Delay_1
-;	return
 
 	end
